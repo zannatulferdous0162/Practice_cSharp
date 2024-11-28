4b65b9e38a14459c7b9466ffa18bddb463339dfb
zannatulferdous0162 <zannatulferdous0162@.com>
2024-11-28 18:51:14 +0600

    Add project files.

---
+++ Input Validation and loop termination/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Validation_and_loop_termination
+{
+    internal class Program
+
+    {
+        public static int CalculateSquare(int num)
+        {
+            return num * num;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Enter the number from 1 to 10 or write Quite exit the APP");
+                string input = Console.ReadLine() != null ? Console.ReadLine() : "";
+                input = input.ToLower().Trim();
+
+                if (input == "quit")
+                {
+                    Console.WriteLine($"Thanks for using the app....GoodBye!!");
+                    break;
+                }
+                else if (!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine($"Please enter a valid input. Please give a number");
+                    continue;
+                }
+                else if (!(number >= 1 && number <= 10))
+                {
+                    Console.WriteLine($"Not a range of  1-10");
+                    continue;
+                }
+                else
+                {
+                    int square = CalculateSquare(number);
+                    Console.WriteLine($"Square of {number}  = {square}");
+                }
+
+            }
+        }
+    }
+}
