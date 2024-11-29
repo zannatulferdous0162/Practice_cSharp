6e75cc9e3f1365e624bacc28e00f293b43b83757
zannatulferdous0162 <zannatulferdous0162@.com>
2024-11-29 20:04:15 +0600

    add getter and setter

---
+++ Setter and Getter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setter_and_Getter
+{
+    class person
+    {
+        public String name; // Readable and writeable
+        private int age; //not readable and not writeable
+
+        public void SetAge(int a)//writeablw
+        {
+            age = a;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+
+          class Test
+        {
+            public static  void Main(string[] args)
+            {
+                person p1 = new person();
+                p1.name = "Zannat";
+                p1.SetAge(11);//writeable
+                Console.WriteLine($"Name : {p1.name}, Age: {p1.GetAge()}\n");
+
+
+            }
+        }
+    }
+}
