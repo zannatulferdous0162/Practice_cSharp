b311b1aa8c08f81ab952cd05071e5b18440a13a8
zannatulferdous0162 <zannatulferdous0162@.com>
2024-11-29 07:56:22 +0600

    add constractor

--- Method class member/Program.cs
+++ Method class member/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Method_class_member
 {
@@ -12,6 +13,28 @@ namespace Method_class_member
         public string name;
         public int age;
 
+        //default constractor
+        public person()
+        {
+            name = "Shaheda";
+            age = 28;
+        }
+        //perameter constractor
+        public person(string n , int a)
+        {
+            name = n;
+            age = a;
+        }
+
+        //Method for get and set the variable
+
+        public void SetValue(string n,int a)
+        {
+            name = n;
+            age = a;
+        }
+
+        //Method create for Display the value 
         public void DisplayInfo()
         {
             Console.WriteLine($"Name : {name}, Age: {age}\n");
@@ -22,17 +45,17 @@ namespace Method_class_member
     {
         static void Main(string[] args)
         {
-            person p1 = new person();
-            p1.name = "Zannatul Ferdous Sheikh";
-            p1.age = 33;
+            person p1 = new person("Zannat",26);//perameter constractor
+           // p1.SetValue("Zannatul Ferdous Sheikh",27);
             p1.DisplayInfo();
 
-            person p2 = new person();
-            p2.name = "Rowshon Sheikh";
-            p2.age = 21;
+            person p2 = new person("Sheikh",33);//perameter constractor
+           // p2.SetValue("Rowshon Sheikh", 23);
             p2.DisplayInfo();
 
-
+            //default constractor
+            person p3 = new person();
+            p3.DisplayInfo();
         }
     }
 }
