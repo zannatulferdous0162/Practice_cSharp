2f1882d95295472faf447e47b2613ec5ce8fd050
zannatulferdous0162 <zannatulferdous0162@.com>
2024-12-09 20:57:16 +0600

    add

---
+++ WpfApp1/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class UserInfo
+    {
+        public class UserInfo
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            //  public string SchoolName { get; set; }
+        }
+
+    }
+}
