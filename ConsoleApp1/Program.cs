39de69f59997481180d3a5d223cf4d3245825766
zannatulferdous0162 <zannatulferdous0162@.com>
2024-12-13 11:04:15 +0600

    add try catch finally

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            try
+            {
+                b = 100 / a;
+                checked
+                {
+                    //int value = int.MaxValue;
+                    //int value = int.MaxValue + 1;
+                    int value = int.MaxValue + int.Parse("1");
+                    Console.WriteLine(value);
+                }
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine("Response from Overflow Exception Block");
+            }
+            catch (DivideByZeroException dze)
+            {
+                Console.WriteLine("Response from Divide By Zero Exception Block");
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine("Response from Invalid Operation Exception Block");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine("Response from Argument Exception Block");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Found");
+            }
+
+            finally
+            {
+                Console.WriteLine("Response from finally Block");
+            }
+        }
+    }
+}
