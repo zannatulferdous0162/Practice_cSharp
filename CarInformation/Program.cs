0d573207d9739c5a2144a063b626e7ceaaba7125
zannatulferdous0162 <zannatulferdous0162@.com>
2024-12-16 22:22:30 +0600

    add  vehical project

---
+++ CarInformation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInformation
+{
+
+    public enum VehicleType
+    { 
+        Car,
+        Motorcycle
+    }
+    public interface IInteriorDesign
+    {
+        List<string> InteriorDesign { get; set; }   
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
