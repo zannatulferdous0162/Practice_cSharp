5254a66467d3b4504ed2575baa2ec00dc606fd9c
zannatulferdous0162 <zannatulferdous0162@.com>
2024-12-31 20:38:19 +0600

    add  student project

---
+++ student management project part-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_management_project_part_1
+{
+
+    public class Student
+    {
+        private string _name;
+        private DateTime _dateOfBirth;
+        private string _rollNo;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public string RollNo
+        {
+            get { return _rollNo; }
+            set { _rollNo = value; }
+        }
+
+        /*Constractor*/
+        public Student(string name,DateTime dateTime, string rollNo)
+        {
+            Name = name;
+            DateOfBirth = dateTime;
+            RollNo = rollNo;
+        }
+
+    }
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
